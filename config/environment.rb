5b13fef994a10bc6bfdbfd7ced1d1e2be68a3473
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-03-11 00:11:43 -0400

    changed the way activerecord connects to database. created database

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,13 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+#ActiveRecord::Base.establish_connection(
+#  :adapter => "sqlite3",
+#  :database => "db/#{ENV['SINATRA_ENV']}.sqlite3"
+#               )
 
+configure :development do
+  set :database, 'sqlite3:db/namedaplace.db'
+end
 require './app/controllers/application_controller'
 require_all 'app'
